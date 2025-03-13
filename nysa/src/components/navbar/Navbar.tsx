a7c0a3f5a96aac6c2a55ebb0a94ad7ec17e38860
Nahuel <nahuelastor@gmail.com>
2025-03-13 19:23:03 -0300

    caratula del navbar mobil mejor adaptado

--- nysa/src/components/navbar/Navbar.tsx
+++ nysa/src/components/navbar/Navbar.tsx
@@ -65,26 +65,26 @@ const Navbar: React.FC = () => {
                     <img src="./img/menu_wax.png" alt="menu" />
                 </div>
                 <div className='content-fold' style={{ display: menuVisibility }}>
-                    <div className={`menu-portada ${themeMode ? 'dark' : ''}`}><img src='./img/menu_vino_page.png' alt="nysa ilustration" /></div>
                     <h2 className='menu-title'>Menu</h2>
+                    <div className={`menu-portada ${themeMode ? 'dark' : ''}`}><img className='menu-portada-img' src='./img/menu_vino_page.png' alt="nysa ilustration" /></div>
                     <ul className='opciones'>
                         <li className='opcion'>
-                            <a href="#quienes_somos" onClick={ isMobile ? handleMenu : undefined }>Quienes Somos</a>
+                            <a href="#quienes_somos" onClick={isMobile ? handleMenu : undefined}>Quienes Somos</a>
                             <img className={`underline ${themeMode ? 'dark' : ''}`} src="./img/under-line-menu.png" alt="underline" />
                         </li>
                         <li className='opcion'>
-                            <a href="#fechas" onClick={ isMobile ? handleMenu : undefined }>Próximos Eventos</a>
+                            <a href="#fechas" onClick={isMobile ? handleMenu : undefined}>Próximos Eventos</a>
                             <img className={`underline ${themeMode ? 'dark' : ''}`} src="./img/under-line-menu.png" alt="underline" />
                         </li>
                         <li className='opcion'>
-                            <a href="#contacto" onClick={ isMobile ? handleMenu : undefined }>Contacto</a>
+                            <a href="#contacto" onClick={isMobile ? handleMenu : undefined}>Contacto</a>
                             <img className={`underline ${themeMode ? 'dark' : ''}`} src="./img/under-line-menu.png" alt="underline" />
                         </li>
                     </ul>
                     <div className={`redes-content ${themeMode ? 'dark' : ''}`}>
                         <Redes />
                     </div>
-                    <div className='theme' onClick={ isMobile ? handleMenu : undefined }>
+                    <div className='theme' onClick={isMobile ? handleMenu : undefined}>
                         <Theme_toggle />
                     </div>
                 </div>
